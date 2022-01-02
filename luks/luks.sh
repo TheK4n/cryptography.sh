74b63c23feb37601514248f104dba6ca171fa205
TheK4n <djvlad967891@gmail.com>
2022-01-02 23:43:13 +0300

    change options luks

--- luks/luks.sh
+++ luks/luks.sh
@@ -6,23 +6,22 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 
-while getopts ":c :o" opt; do
 
-    case $opt in
-      c)
-        sudo umount /mnt/Files &>/dev/null && sudo cryptsetup luksClose Files || echo "luks: Already unmounted" >&2
-        exit 0
-        ;;
-      o)
-        sudo cryptsetup luksOpen /dev/sdb5 Files && sudo mount /dev/mapper/Files /mnt/Files && echo "luks: Successfully decrypted"
-        exit 0
-        ;;
-      \?)
-        echo "luks: Invalid option: -$OPTARG" >&2 && exit 2
-        ;;
-    esac
+case "$1" in
+  "close")
+    sudo umount /mnt/Files &>/dev/null && sudo cryptsetup luksClose Files || echo "luks: Already unmounted" >&2
+    exit 0
+    ;;
+  "open")
+    sudo cryptsetup luksOpen /dev/sdb5 Files && sudo mount /dev/mapper/Files /mnt/Files && echo "luks: Successfully decrypted"
+    exit 0
+    ;;
+  \?)
+    echo "luks: Invalid option: $1" >&2 && exit 2
 
-done
+    ;;
+esac
 
-echo "luks: No option, -o to open, -c to close" >&2 && exit 3
+
+echo "luks: No option, open, close" >&2 && exit 3
 
